0f4d05afa15c669a19bec1b16eaaa3a938ccf3d7
raul <raulchavezjr7@gmail.com>
2026-01-12 00:18:32 -0500

    added emtion detection

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = myobj, headers=header)
+    formatted_response = json.loads(response.text)
+    scores = {
+        'anger': formatted_response['emotionPredictions'][0]['emotion']['anger'],
+        'disgust':formatted_response['emotionPredictions'][0]['emotion']['disgust'],
+        'fear': formatted_response['emotionPredictions'][0]['emotion']['fear'],
+        'joy': formatted_response['emotionPredictions'][0]['emotion']['joy'],
+        'sadness': formatted_response['emotionPredictions'][0]['emotion']['sadness']}
+    dominant_emotion = max(scores, key=scores.get)
+    return {'anger': scores['anger'],
+        'disgust': scores['disgust'],
+        'fear': scores['fear'],
+        'joy': scores['joy'],
+        'sadness': scores['sadness'],
+        'dominant_emotion': dominant_emotion}
