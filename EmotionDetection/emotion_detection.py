9fb9d3871925b5f7b5b4195acd4397127dde1501
raul <raulchavezjr7@gmail.com>
2026-01-12 01:33:49 -0500

    Finished project and 10/10 pylint

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,16 +7,29 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
-    scores = {
-        'anger': formatted_response['emotionPredictions'][0]['emotion']['anger'],
-        'disgust':formatted_response['emotionPredictions'][0]['emotion']['disgust'],
-        'fear': formatted_response['emotionPredictions'][0]['emotion']['fear'],
-        'joy': formatted_response['emotionPredictions'][0]['emotion']['joy'],
-        'sadness': formatted_response['emotionPredictions'][0]['emotion']['sadness']}
-    dominant_emotion = max(scores, key=scores.get)
-    return {'anger': scores['anger'],
-        'disgust': scores['disgust'],
-        'fear': scores['fear'],
-        'joy': scores['joy'],
-        'sadness': scores['sadness'],
-        'dominant_emotion': dominant_emotion}
+
+    return_json = {}
+
+    if response.status_code == 304 or response.status_code == 400:
+        return_json = {'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None}
+    else:
+        scores = {
+            'anger': formatted_response['emotionPredictions'][0]['emotion']['anger'],
+            'disgust':formatted_response['emotionPredictions'][0]['emotion']['disgust'],
+            'fear': formatted_response['emotionPredictions'][0]['emotion']['fear'],
+            'joy': formatted_response['emotionPredictions'][0]['emotion']['joy'],
+            'sadness': formatted_response['emotionPredictions'][0]['emotion']['sadness']}
+        dominant_emotion = max(scores, key=scores.get)
+        return_json = {'anger': scores['anger'],
+            'disgust': scores['disgust'],
+            'fear': scores['fear'],
+            'joy': scores['joy'],
+            'sadness': scores['sadness'],
+            'dominant_emotion': dominant_emotion}
+
+    return return_json
